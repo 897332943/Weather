07bff365f49e374aecd8309fcc6982e1a0d00235
Your Name <you@example.com>
2018-01-02 11:38:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/weather/json/Sport.java
@@ -0,0 +1,30 @@
+/**
+  * Copyright 2018 bejson.com 
+  */
+package com.example.weather.json;
+
+/**
+ * Auto-generated: 2018-01-02 11:35:22
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+public class Sport {
+
+    private String brf;
+    private String txt;
+    public void setBrf(String brf) {
+         this.brf = brf;
+     }
+     public String getBrf() {
+         return brf;
+     }
+
+    public void setTxt(String txt) {
+         this.txt = txt;
+     }
+     public String getTxt() {
+         return txt;
+     }
+
+}
